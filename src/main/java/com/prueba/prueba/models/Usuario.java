e0bbb4c123efcc52fcb50dd633870e1651848f41
jasencio92 <jorge.asencio92@gmail.com>
2024-07-31 07:49:59 -0600

    avances

--- src/main/java/com/prueba/prueba/models/Usuario.java
+++ src/main/java/com/prueba/prueba/models/Usuario.java
@@ -1,9 +1,6 @@
 package com.prueba.prueba.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
